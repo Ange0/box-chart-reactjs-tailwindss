e6405877a10d94affde7b77b709e6518973c0045
ange0 <kouassiangey@gmail.com>
2023-01-10 15:38:58 +0000

    Aj

--- src/App.jsx
+++ src/App.jsx
@@ -35,14 +35,14 @@ function App() {
         {/* header box chat */}
         <div className='flex justify-between text-white p-4 bg-gray-900/10 backdrop-blur-2xl rounded-tl-2xl rounded-tr-2xl fixed w-[400px]'>
           <div className='font-bold text-xl'>Messages</div>
-          <div className=''>
-              <button>
-                <svg viewBox="0 0 24 24" aria-hidden="true"  fill="currentColor" className="w-5 h-5">
+          <div className='flex space-x-2'>
+              <button className='hover:bg-[#fff]/20 h-9 w-9 p-2 rounded-full transition duration-500'>
+                <svg viewBox="0 0 24 24" aria-hidden="true"  fill="currentColor" className="w-full h-full">
                   <g><path d="M1.998 5.5c0-1.381 1.119-2.5 2.5-2.5h15c1.381 0 2.5 1.119 2.5 2.5V12h-2v-1.537l-8 3.635-8-3.635V18.5c0 .276.224.5.5.5H13v2H4.498c-1.381 0-2.5-1.119-2.5-2.5v-13zm2 2.766l8 3.635 8-3.635V5.5c0-.276-.224-.5-.5-.5h-15c-.276 0-.5.224-.5.5v2.766zM19 18v-3h2v3h3v2h-3v3h-2v-3h-3v-2h3z"></path></g>
                 </svg>
               </button>
-              <button className='ml-4'>
-                <svg viewBox="0 0 24 24" aria-hidden="true" fill="currentColor" className="w-5 h-5">
+              <button className='hover:bg-[#fff]/20 h-9 w-9 p-2 rounded-full transition duration-500'>
+                <svg viewBox="0 0 24 24" aria-hidden="true" fill="currentColor" className="w-full h-full">
                     <g><path d="M12 11.59L3.96 3.54 2.54 4.96 12 14.41l9.46-9.45-1.42-1.42L12 11.59zm0 7l-8.04-8.05-1.42 1.42L12 21.41l9.46-9.45-1.42-1.42L12 18.59z"></path></g>
                 </svg>
               </button>
@@ -52,10 +52,10 @@ function App() {
 
         {/* content  box chat */}
         <div className='overflow-y-auto rounded-tl-2xl rounded-tr-2xl'>
-            <div className='h-20 bg-blackmb-2'></div>
+            <div className='h-[4.3rem] bg-blackmb-2'></div>
           {/* items users */}
             <div className='flex flex-col divide-y divide-stone-700'>
-                <div className='flex  p-4 space-x-4 hover:bg-[#16181c] cursor-pointer transition'>
+                <div className='flex  p-4 space-x-4 hover:bg-[#16181c] cursor-pointer transition duration-200'>
                   <img className='h-12 w-12 rounded-full' src="https://flowbite.com/docs/images/people/profile-picture-1.jpg" />
                   <div className='flex text-[#71767b]  justify-between flex-grow'>
                       <div className='flex flex-col'>
@@ -79,7 +79,7 @@ function App() {
                   </div>
                 </div>
 
-                <div className='flex  p-4 space-x-4 hover:bg-[#16181c] cursor-pointer transition'>
+                <div className='flex  p-4 space-x-4 hover:bg-[#16181c] cursor-pointer transition duration-200'>
                   <img className='h-12 w-12 rounded-full' src="https://flowbite.com/docs/images/people/profile-picture-2.jpg" />
                   <div className='flex text-[#71767b]  justify-between flex-grow'>
                       <div className='flex flex-col'>
